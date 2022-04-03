7f9c992d48d92194b36229a3cd76ff45660c2b2a
Ruslan <ruslan.shtybaev@gmail.com>
2022-04-03 14:15:57 +0300

    Initial Commit

---
+++ LiveCiclesOfView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveCiclesOfView
+//
+//  Created by Руслан Штыбаев on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
